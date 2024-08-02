09e1fbd7000d7c9f11c1ff1450482515b219476b
Lastknight12 <108629475+Lastknight12@users.noreply.github.com>
2024-08-03 00:05:16 +0300

    Update assets.php

--- api/assets.php
+++ api/assets.php
@@ -1,40 +1,13 @@
 <?php
 
-// Handle image requests
-if (isset($_GET['file'])) {
-    // Serve image files
-    $file = basename($_GET['file']);
-    $filePath = __DIR__ . '/images/' . $file;
-
-    if (file_exists($filePath)) {
-        $extension = pathinfo($file, PATHINFO_EXTENSION);
-
-        // Set appropriate Content-Type based on file extension
-        switch ($extension) {
-            case 'jpg':
-            case 'jpeg':
-                header('Content-Type: image/jpeg');
-                break;
-            case 'png':
-                header('Content-Type: image/png');
-                break;
-            case 'gif':
-                header('Content-Type: image/gif');
-                break;
-            case 'webp':
-                header('Content-Type: image/webp');
-                break;
-            default:
-                header('HTTP/1.1 415 Unsupported Media Type');
-                echo 'Unsupported image type';
-                exit;
-        }
-
-        readfile($filePath);
-        exit; // Ensure no further code is executed
-    } else {
-        header('HTTP/1.1 404 Not Found');
-        echo 'Image not found';
-        exit;
-    }
+/**
+ * Built assets aren't currently routeable via vercel-php
+ * Manually route assets to be found
+ */
+if ($_GET['type'] === 'css') {
+    header("Content-type: text/css; charset: UTF-8");
+    echo require __DIR__ . '/../css/' . basename($_GET['file']);
+} else if ($_GET['type'] === '*') {
+    header('Content-Type: application/javascript; charset: UTF-8');
+    echo require __DIR__ . '/../js/' . basename($_GET['file']);
 }
